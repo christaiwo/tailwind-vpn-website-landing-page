47d2e7355d04e4c565d7dcc3f3a23bca7a94d196
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-04-11 12:09:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# source https://www.figma.com/community/file/858999227165747995/FREEBIES-Landingpage-LaslesVPN
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Rubik' : ["Rubik Storm", "cursive"],
+        'Merriweather' : ['Merriweather', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
